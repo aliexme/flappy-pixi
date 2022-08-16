57eee1ff31552d104aa18f27c4b3e6b7a750c176
Alexander Smirnov <al.smirnov996@gmail.com>
2022-08-16 17:53:54 +0300

    chore: setup pixi.js

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
+import * as PIXI from 'pixi.js'
+
+import eggHead from './assets/eggHead.png'
+
+const app = new PIXI.Application()
+
+document.body.appendChild(app.view)
+
+const sprite = PIXI.Sprite.from(eggHead)
+app.stage.addChild(sprite)
+
+let elapsed = 0.0
+app.ticker.add((delta) => {
+  elapsed += delta
+  sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0
+})
+
 export {}
